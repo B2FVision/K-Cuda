12a25fbbca96babb9ae4e61b5dc06c06604b99c3
B2FVision <90093927+B2FVision@users.noreply.github.com>
2024-11-06 05:42:08 -0300

    Update 67_6.py

--- 67_6.py
+++ 67_6.py
@@ -39,7 +39,7 @@ def salvar_subrange(subrange_start, subrange_end):
 # Função para executar o KeyHunt
 def executar_keyhunt(subrange_start, subrange_end):
     comando = [
-        "./KeyHunt", "--gpu", "-m", "address", address,
+        "./KeyHunt", "--gpu", "--gpui", "0,1,2,3,4,5,6,7", "-m", "address", address,
         "--range", f"{subrange_start}:{subrange_end}",
         "--coin", "BTC", "-o", output_file,
     ]

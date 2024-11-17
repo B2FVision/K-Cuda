ca758913017575a8dc9cb5d9ceb8fcb45899515d
B2FVision <90093927+B2FVision@users.noreply.github.com>
2024-11-17 06:11:51 -0300

    Update 67_ordem_v1.3.py

--- 67_ordem_v1.3.py
+++ 67_ordem_v1.3.py
@@ -4,8 +4,8 @@ import subprocess
 from datetime import datetime
 
 # Parâmetros base
-start_range = int("5012682468d2007cf", 16)  # Converte para inteiro
-end_range = int("50170ef9ba352fd44", 16)    # Converte para inteiro
+start_range = int("517c98fffffffffff", 16)  # Converte para inteiro
+end_range = int("517ca8fffffffffff", 16)    # Converte para inteiro
 address = "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9"
 output_file = "viva.txt"
 log_file = "saveit.tsv"
